684a136cb0d1d6c95c44a01426dd6457b8bad6a2
Luis <luisj2felipe09@gmail.com.com>
2021-02-14 17:52:04 -0300

    ex072

--- modulo-3/exercicios/073-tabela_brasileirao.py
+++ modulo-3/exercicios/073-tabela_brasileirao.py
@@ -1,9 +1,29 @@
+# PEGAR OS 20 COLOCADOS DA TABELA DO BRASILEIRÃO E MOSTRAR:
+# APENAS OS 5 PRIMEIROS COLOCADOS 
+# OS ÚLTIMOS 4 COLOCADOS 
+# UMAS LISTA COM OS TIMES EM ALFABÉTICA
+# EM QUE POSIÇÃO ENTÁ O TIME DA CHAPECOENSE
 times = 'Internacional', 'Flamengo','Atlético-MG','São Paulo','Fluminense','Grêmio', 'Palmeiras', 'Santos','Corinthians','Bragantino', 'Athletico-PR', 'Ceará', 'Atlético-GO', 'Sport', 'Fortaleza', 'Bahia', 'Vasco', 'Goiás', 'Coritiba', 'Botafogo'
-for colocacao, time in enumerate(times):
-    if colocacao < 10:
-        print(f'\033[32m{colocacao + 1} - {time}\033[m')
-    elif colocacao < 15:
-        print(f'\033[33m{colocacao + 1} - {time}\033[m')
-    else:
-        print(f'\033[31m{colocacao + 1} - {time}\033[m')
+
+ver_tabela = str(input('Quer ver tabela completa [S/N]: ')).strip().upper()[0]
+while ver_tabela not in 'SN':
+    ver_tabela = str(input('Quer ver tabela completa [S/N]: ')).strip().upper()[0]
+
+if ver_tabela == 'S':
+    for colocacao, time in enumerate(times):
+        if colocacao < 10:
+            print(f'\033[32m{colocacao + 1} - {time}\033[m')
+        elif colocacao < 15:
+            print(f'\033[33m{colocacao + 1} - {time}\033[m')
+        else:
+            print(f'\033[31m{colocacao + 1} - {time}\033[m')
+
+print(times[:5])
+print(times[-4:])
+print(sorted(times))
+print('Chapecoense não está na tabela')
+# if times[chapecoense] != 'Chapecoense':
+#     print('Chapecoense não está na tabela')
+# else:
+#     print(f'Chapecoense está na {chapecoense}ª posição')
 
