85dcf45b40f9e1bf2cc0a8b6818016d9887ff7e3
Luis <luisj2felipe09@gmail.com.com>
2021-02-14 23:18:42 -0300

    Update 075-analisando-dados_tupla.py

--- modulo-3/exercicios/075-analisando-dados_tupla.py
+++ modulo-3/exercicios/075-analisando-dados_tupla.py
@@ -1,12 +1,17 @@
-# FAZER UM PROGAMA QUE LEIA QUATRO VALORES E GUARDE EM UMA TUPLA
+# FAZER UM PROGAMA QUE LEIA QUATRO VALORES E GUARDE EM UMA TUPLA E MOSTRE
+# QUANTAS VEZES CADA QUE O 9 APARECEU
+# EM QUE POSIÇÃO O 3 APARECE
+# QUANTOS VALORES PARES FORAM MODIFICADO 
 
-t = '',
+# !MODIFIQUEI A SINTAXE COM BASE NA RESOLUÇÃO!
+# SIM, PODEMOS FAZER UMA TUPLA COM VARIOS INPUT
+n = (int(input("Digite um número: ")), 
+    int(input("Digite um número: ")), int(input("Digite um número: ")), 
+    int(input("Digite um número: ")))
+
 cont9 = cont_par = p3 = 0
-for x in range(1, 5):
-    n = int(input("Digite um número: ")),
-    t += n
-print(f'Você digitou os valores {t[1:]}')
-for pos, x in enumerate(t):
+
+for pos, x in enumerate(n):
     if x == 9:
         cont9 += 1
     if x == 3:
@@ -14,10 +19,12 @@ for pos, x in enumerate(t):
     if pos >= 1:
         if x % 2 == 0:
             cont_par += 1
-        
+
+
+print(f'Você digitou os números: {n}')
 print(f'O 9 apareceu {cont9} vezes')
 if p3 > 0:
-    print(f'O 3 está na posição {t.index(3)}ª posição')
+    print(f'O 3 está na posição {n.index(3)}ª posição')
 else:
     print('Você não digite nenhum 3')
 print(f'Valores pares digitados foram {cont_par}')
