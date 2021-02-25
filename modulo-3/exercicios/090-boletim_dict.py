46ec2a9032ebb8ba02f73daf8aeaa59b87052a5c
Luis Felipe N <luisj2felipe09@gmail.com>
2021-02-25 15:52:29 -0300

    Terminar progama

--- modulo-3/exercicios/090-boletim_dict.py
+++ modulo-3/exercicios/090-boletim_dict.py
@@ -2,17 +2,19 @@ pessoas = []
 dados = dict()
 
 while True:
+    print('-' * 30)
     dados['nome'] = str(input('Nome: '))
     dados['media'] = float(input(f'Média de {dados["nome"]}: '))
-    if dados['media'] <= 3:
+    if dados['media'] <= 4.9:
         dados['situacao'] = '\033[31mReprovado\033[m'
-    elif dados['media'] <= 6:
+    elif dados['media'] <= 6.9:
         dados['situacao'] = '\033[33mRecuperação\033[m'
     else:
         dados['situacao'] = '\033[32mAprovado\033[m'
     
     pessoas.append(dados.copy())
 
+    print('-' * 30)
     continuar = str(input('deseja continuar [S/N]: ')).strip().lower()[0]
     while continuar not in 'sn':
         continuar = str(input('deseja continuar [S/N]: ')).strip().lower()[0]
--- modulo-3/exercicios/095-aproveitamento.v2.py
+++ modulo-3/exercicios/095-aproveitamento.v2.py
@@ -3,15 +3,15 @@ gols_partida = []
 dados_jogador = {}
 
 while True:
-    print('-' * 30)
-    dados_jogador['nome'] = str(input('Nome: '))
-    quan_jogos = int(input(f'Quantos partidas {dados_jogador["nome"]} jogou: '))
+    print('-' * 34)
+    dados_jogador['nome'] = str(input('Nome: '))# PEGANDO O NOME
+    quan_jogos = int(input(f'Quantos partidas {dados_jogador["nome"]} jogou: '))# PEGANDO QUANTIDADE DE PARTIDA JOGADAS
 
     for partida in range(1, quan_jogos + 1):
-        gols_partida.append(int(input(f'Quantos gols {dados_jogador["nome"]} fez na {partida}ª partida: ')))
+        gols_partida.append(int(input(f'Quantos gols {dados_jogador["nome"]} fez na {partida}ª partida: ')))# PEGANDO GOLS MARCADOS
 
-    dados_jogador['gols'] = gols_partida.copy()
-    dados_jogador['total'] = sum(gols_partida)
+    dados_jogador['gols'] = gols_partida.copy()# ADICIONANDO GOLS MARCADOS
+    dados_jogador['total'] = sum(gols_partida)# ADICIONANDO TOTAL DE GOLS
     gols_partida.clear()
 
     jogadores.append(dados_jogador.copy())
@@ -27,7 +27,15 @@ print(f'{"cod":<4}{"Nome":<10}{"gols":<15}{"Total":<5}')
 print('-' * 34)
 
 for e, jogador in enumerate(jogadores):
-    print(f'{e:<4}{jogador["nome"]:<10}{jogador["gols"]}{"":<{12 - len(jogador["gols"]) }}{jogador["total"]}')
-    # print(f'{e:<4}')
-    # for v in jogador.values():
-    #     print(f'{v:<15}')
+    print(f'{e:<4}{jogador["nome"]:<10}{str(jogador["gols"]):<17}{jogador["total"]:<5}')
+
+print('-' * 34)
+while True:
+    i = int(input('Qual jogador deseja ver (999 parar):'))
+    print(f'''
+        O nome do jogador é {jogadores["nome"][i]}
+        {jogadores["gols"][i]} fez gols
+        No total foram {jogadores["total"][i]} gols
+        '''
+    )
+
